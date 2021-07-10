0e68a5fb2a705601c7f5592c5489ab5eafebb750
Tsvetan <dukov.tzvetan@gmail.com>
2021-07-10 21:13:52 +0300

    Commit 08

--- EcommerceAutomationTests/pageObjects/login.page.js
+++ EcommerceAutomationTests/pageObjects/login.page.js
@@ -9,6 +9,25 @@ class LoginPage extends Page {
     get createAccountFormHeader() { return $('#create-account_form h3') };
     get createAccountEmailField() { return $('#email_create') };
     get createAccountButton() { return $('#SubmitCreate') };
+    get accountFormPageHeader() { return $('.page-heading') };
+    get genderMaleRadioButton() { return $("#id_gender1") };
+    get genderFemaleRadioButton() { return $("#id_gender2") };
+    get firstNameField() { return $("#customer_firstname") };
+    get lastNameField() { return $("#customer_lastname") };
+    get emailField() { return $("#email") };
+    get passwordField() { return $("#passwd") };
+    get passwordCharAdvise() { return $("//div[@class = 'required password form-group']/child::span") };
+    get dayOfBirthDropdownMenu() { return $("#uniform-days") };
+    get monthOfBirthDropdown() { return $("#uniform-months") };
+    get yearOfBirthDropdown() { return $("#uniform-years") };
+    get newsletterCheckbox() { return $("#uniform-newsletter") };
+    get newsletterSelectedCheckbox() { return $(".checker .checked") };
+    get recieveSpecialOffersCheckbox() { return $("#uniform-optin") };
+    get recievedSpecialOffersSelectedCheckbox() { return $("div[id = 'uniform-optin'] span") };
+    get addressFirstNameField() { return $("#firstname") };
+    get addressLastNameField() { return $("#lastname") };
+    get addressCompanyField() { return $("#company") };
+
 
     open() {
         return super.open();
@@ -58,5 +77,182 @@ class LoginPage extends Page {
         this.createAccountButton.waitForDisplayed();
         this.createAccountButton.click();
     }
+
+    /**
+     * Get create an account page header
+     * @returns {String} Create an account form page header
+     */
+    getAccountFormPageHeadingText() {
+        this.accountFormPageHeader.waitForDisplayed();
+        return this.accountFormPageHeader.getText();
+    }
+
+    selectGenderMaleOption() {
+        this.genderMaleRadioButton.waitForDisplayed();
+        this.genderMaleRadioButton.click();
+    }
+
+    selectGenderFemaleOption() {
+        this.genderFemaleRadioButton.waitForDisplayed();
+        this.genderFemaleRadioButton.click();
+    }
+
+    /**
+     * Enter first name in create account field
+     * @param {String} firstName 
+     */
+    setFirstNameField(firstName) {
+        this.firstNameField.waitForDisplayed();
+        this.firstNameField.setValue(firstName);
+    }
+
+    /**
+     * Enter last name in create account field
+     * @param {String} lastName 
+     */
+    setLastNameField(lastName) {
+        this.lastNameField.waitForDisplayed();
+        this.lastNameField.setValue(lastName);
+    }
+
+    /**
+     * Get email field text, email is set automatically
+     * @returns {String} Email field text
+     */
+    checkEmailFieldText() {
+        this.emailField.waitForDisplayed();
+        return this.emailField.getValue();
+    }
+
+    /**
+     * Enter password in password field
+     * @param {String} password Desired user password
+     */
+    setPasswordField(password) {
+        this.passwordField.waitForDisplayed();
+        this.passwordField.setValue(password);
+    }
+
+    /**
+     * Verify text below password field
+     * @returns {String} Advise text for password creation
+     */
+    checkPasswordCharAdviseText() {
+        this.passwordCharAdvise.waitForDisplayed();
+        return this.passwordCharAdvise.getText();
+    }
+
+    /**
+     * Select day of birth from select dropdown
+     * @param {Number} dayOfBirth User day of birth
+     */
+    selectDayOfBirth(dayOfBirth) {
+        this.dayOfBirthDropdownMenu.waitForDisplayed();
+        this.dayOfBirthDropdownMenu.click();
+        this.dayOfBirthDropdownMenu.selectByIndex(dayOfBirth);
+    }
+
+    /**
+     * Select month of birth from select dropdown
+     * @param {Number} monthOfBirth User month of birth
+     */
+    selectMonthOfBirth(monthOfBirth) {
+        this.monthOfBirthDropdown.waitForDisplayed();
+        this.monthOfBirthDropdown.click();
+        this.monthOfBirthDropdown.selectByIndex(monthOfBirth);
+    }
+
+    /**
+     * Select year of birth from select dropdown
+     * @param {Number} yearOfBirth User year of birth
+     */
+    selecYearOfBirth(yearOfBirth) {
+        this.yearOfBirthDropdown.waitForDisplayed();
+        this.yearOfBirthDropdown.click();
+        this.yearOfBirthDropdown.selectByIndex(yearOfBirth);
+    }
+
+    selectNewsletterCheckbox() {
+        this.newsletterCheckbox.waitForDisplayed();
+        this.newsletterCheckbox.click();
+    }
+
+    /**
+     * Verify that checkbox is selected
+     * @returns {String} Checkbox attribute value, must equals "checked"
+     */
+    newsletterCheckboxIsSelected() {
+        this.newsletterSelectedCheckbox.waitForDisplayed();
+        return this.newsletterSelectedCheckbox.getAttribute('class');
+    }
+
+    selectSpecialOffersCheckbox() {
+        this.recieveSpecialOffersCheckbox.waitForDisplayed();
+        this.recieveSpecialOffersCheckbox.click();
+    }
+
+    /**
+     * Verify that checkbox is selected
+     * @returns {String} Checkbox attribute value, must equals "checked"
+     */
+    recieveSpecialOffersCheckboxIsSelected() {
+        this.recievedSpecialOffersSelectedCheckbox.waitForDisplayed();
+        return this.recievedSpecialOffersSelectedCheckbox.getAttribute('class');
+    }
+
+    /**
+     * Enter user first name in 'Your address' form
+     * @param {String} addressFirstName User first name in 'Your address' form
+     */
+    setAddressFirstNameField(addressFirstName) {
+        this.addressFirstNameField.waitForDisplayed();
+        this.addressFirstNameField.setValue(addressFirstName);
+    }
+
+    /**
+     * Enter user last name in 'Your address' form
+     * @param {String} addressLastName User last name in 'Your address' form
+     */
+    setAddressLastNameField(addressLastName) {
+        this.addressLastNameField.waitForDisplayed();
+        this.addressLastNameField.setValue(addressLastName);
+    }
+
+    /**
+     * Enter user company name in 'Your address' form
+     * @param {String} addressCompanyName User company name in 'Your address' form
+     */
+    setAddressCompanyNameField(addressCompanyName) {
+        this.addressCompanyField.waitForDisplayed();
+        this.addressCompanyField.setValue(addressCompanyName);
+    }
+
+    /**
+     * Verify correct data is entered into the field
+     * @returns {String} First name field text in 'Your address'
+     */
+    checkAddressFirstNameFieldText() {
+        this.addressFirstNameField.waitForDisplayed();
+        return this.addressFirstNameField.getValue();
+    }
+
+    /**
+     * Verify correct data is entered into the field
+     * @returns {String} Last name field text in 'Your address'
+     */
+    checkAddressLastNameFieldText() {
+        this.addressLastNameField.waitForDisplayed();
+        return this.addressLastNameField.getValue();
+    }
+
+    /**
+     * Verify correct data is entered into the field
+     * @returns {String} Company name field text in 'Your address'
+     */
+    checkAddressCompanyNameFieldText() {
+        this.addressCompanyField.waitForDisplayed();
+        return this.addressCompanyField.getValue();
+    }
+
 }
 module.exports = new LoginPage();
--- EcommerceAutomationTests/test/loginPageTests.spec.js
+++ EcommerceAutomationTests/test/loginPageTests.spec.js
@@ -1,7 +1,8 @@
 const homePage = require('../pageObjects/home.page')
 const loginPage = require('../pageObjects/login.page')
 const { assert } = require("chai")
-const input = require('../testData/createAccountEmailData')
+const input = require('../testData/createAccountEmailData');
+const { checkEmailFieldText } = require('../pageObjects/login.page');
 
 describe("Login page tests", () => {
     beforeEach(() => {
@@ -14,7 +15,7 @@ describe("Login page tests", () => {
         assert.equal('AUTHENTICATION', loginPage.getPageHeader(), "Incorrect page header!");
     });
 
-    it("Verify 'Create and account form header'", () => {
+    it("Verify 'Create an account form header'", () => {
         homePage.open();
         homePage.clickOnSignInLink();
         assert.equal('CREATE AN ACCOUNT', loginPage.getCreateAccountFormHeader(), "Incorrect form header!");
@@ -29,6 +30,33 @@ describe("Login page tests", () => {
         loginPage.clickCreateAccountButton();
         /**
          * TODO define locators and functions for 'Create an account' form
+         */
+        assert.equal("CREATE AN ACCOUNT", loginPage.getCreateAccountFormHeader(), "Incorrect header!");
+        //Set fields in 'Create an account form'
+        loginPage.selectGenderMaleOption();
+        loginPage.setFirstNameField("First Name");
+        loginPage.setLastNameField("Last Name");
+        assert.equal("test.automation.egt@gmail.com", loginPage.checkEmailFieldText(), "Incorrect email");
+        loginPage.setPasswordField("Password");
+        assert.equal("(Five characters minimum)", loginPage.checkPasswordCharAdviseText(), "Incorrect text!");
+        loginPage.selectDayOfBirth(11);
+        loginPage.selectMonthOfBirth(9);
+        loginPage.selecYearOfBirth(35);
+        //Select both checkboxes and verify that both are selected
+        loginPage.selectNewsletterCheckbox();
+        assert.equal("checked", loginPage.newsletterCheckboxIsSelected(), "Checkbox is not selected!");
+        loginPage.selectSpecialOffersCheckbox();
+        assert.equal("checked", loginPage.recieveSpecialOffersCheckboxIsSelected(), "Checkbox is not selected!");
+        //Set fields in 'Your address' and verify correct information is send
+        loginPage.setAddressFirstNameField("Address First name");
+        assert.equal("Address First name", loginPage.checkAddressFirstNameFieldText(), "Incorrect first name data!");
+        loginPage.setAddressLastNameField("Address Last name");
+        assert.equal("Address Last name", loginPage.checkAddressLastNameFieldText(), "Incorrect last name data!");
+        loginPage.setAddressCompanyNameField("Address Company name");
+        assert.equal("Address Company name", loginPage.checkAddressCompanyNameFieldText(), "Incorrect company data!");
+
+        /**
+         * TODO: Create more functions for 'Create account' form
          */
     });
 });
